71a6782ae60683a482afcf589454d76c688b33ae
Gabrielle Bhagwat <gabriellebhagwat@gabrielles-air.mynetworksettings.com>
2024-07-05 17:50:40 -0400

    add component state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Gabrielle'
+    };
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hey Gabrielle</p>
+          <p>Hey {this.state.name}</p>
           <button>Change Name</button>
         </header>
       </div>
