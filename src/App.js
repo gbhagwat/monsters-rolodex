09883f12df6541d808af60449194da6c082ae8c1
Gabrielle Bhagwat <gabriellebhagwat@gabrielles-air.mynetworksettings.com>
2024-07-05 17:45:45 -0400

    convert function to class in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey Gabrielle</p>
-        <button>Change Name</button>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hey Gabrielle</p>
+          <button>Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
